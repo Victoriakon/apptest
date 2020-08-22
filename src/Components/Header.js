184a59997a2ebbc8112b936d0216d78afee3ba1b
victoria <larikon@hotmail.com>
2020-08-22 17:57:17 +0300

    appdate Header.js
    New name of Application

---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header(){
+return (
+    <header className="border-b p-8 flex justify-between item-center">
+    
+        AppNameApp With using git hub
+   </header>     
+)
+
+
+}
+export default Header
